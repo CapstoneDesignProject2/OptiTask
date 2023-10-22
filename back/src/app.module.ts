ee8b540a795ecfd13196232d9f206704c4c1c01c
kalteon <tnghdjaak75@gmail.com>
2023-10-22 22:41:28 +0900

    feat: module and direction service, controller

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { DirectionModule } from './direction/direction.module';
+import { ActivityModule } from './activity/activity.module';
+import { PostModule } from './post/post.module';
+import { ReportModule } from './report/report.module';
+import { ConsultingModule } from './consulting/consulting.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule, DirectionModule, ActivityModule, PostModule, ReportModule, ConsultingModule],
   controllers: [AppController],
   providers: [AppService],
 })
