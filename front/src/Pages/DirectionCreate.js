7f37773175638610311d0472156598a020494afa
BusanGukbap <dong2857210@gmail.com>
2023-10-30 17:22:13 +0900

    fix: fix file name
    PurposeCreate -> DirectionCreate로 파일명 변경 및 해당하는 코드들도 이에 맞게 수정함

--- front/src/Pages/DirectionCreate.js
+++ front/src/Pages/DirectionCreate.js
@@ -1,8 +1,8 @@
 import React, { useEffect, useState} from "react";
 import {useNavigagte} from 'react-router-dom';
 
-function PurposeCreate({}) {
+function DirectionCreate({}) {
     
 }
 
-export default PurposeCreate;
+export default DirectionCreate;
