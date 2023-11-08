2fdc6d8beb983c9073cdb795270f20fb8d7ec420
kalteon <tnghdjaak75@gmail.com>
2023-11-08 22:33:25 +0900

    test: findWeeklyActivity

--- back/src/activity/activity-dto.ts
+++ back/src/activity/activity-dto.ts
@@ -1,5 +1,4 @@
 export class CreateActivityRequest {
-  id: number;
   startTime: Date;
   endTime: Date;
   content: string;
--- back/src/activity/activity.service.spec.ts
+++ back/src/activity/activity.service.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ActivityService } from './activity.service';
+import { CreateActivityRequest } from './activity-dto';
 
 describe('ActivityService', () => {
   let service: ActivityService;
@@ -15,4 +16,27 @@ describe('ActivityService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  function createActivitiesForWeek(service: ActivityService) {
+    for (let i = 0; i < 7; i++) {
+      const activity = new CreateActivityRequest();
+      activity.startTime = new Date();
+      activity.startTime.setDate(activity.startTime.getDate() - i);
+      activity.endTime = new Date(activity.startTime);
+      activity.endTime.setHours(activity.endTime.getHours() + 1);
+      activity.content = `Activity ${i + 1}`;
+      activity.tag = `Tag ${i + 1}`;
+      activity.success = [`Success ${i + 1}`];
+  
+      service.createActivity(activity);
+    }
+  }
+  describe('findWeeklyActivity', () => {
+    it('should return activities for the past week', () => {
+      createActivitiesForWeek(service);
+  
+      const activities = service.findWeeklyActivity(1);
+      expect(activities.length).toBe(7);
+    });
 });
+})
