3d2a78facb6f8d88ace6380692557458d46a552b
us4c0d3 <jwseok312@gmail.com>
2023-11-27 13:57:47 +0900

    Fix: 환경변수 통일 문제 및 DB 접속 클라이언트 버전 수정 mysql 8.0부터 authentication protocol이 달라져 버전 변경이 필요하여 추가 설치 및 기존 mysql client 삭제

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -15,11 +15,11 @@ import { ConsultingModule } from './consulting/consulting.module';
     ConfigModule.forRoot(), // .env
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: process.env.DATABASE_HOST,
-      port: parseInt(process.env.DATABASE_PORT, 10) || 3306,
-      username: process.env.DATABASE_USERNAME,
-      password: process.env.DATABASE_PASSWORD,
-      database: process.env.DATABASE_NAME,
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT, 10) || 3306,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
       synchronize: true,
     }),
     UserModule,
