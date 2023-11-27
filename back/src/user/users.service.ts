ac032d106d8d7d8011587a3becd7d94b078a7ba5
us4c0d3 <jwseok312@gmail.com>
2023-11-27 15:18:40 +0900

    FEAT: 회원가입 및 로그인, JWT 유저 검증 구현 * UserModule   * 엔티티가 있어 실제로 DB에 접근하는 곳. 현재는 회원가입 기능만 구현된 상태. Post타입 signup
    * AuthModule
      * JWT 발급 및 검증과 관련된 모듈
      * 로그인 시 토큰이 발급됨. Post타입 login
      * 토큰 검증은 에 로 구현은 해둔 상태이나 controller에 연결해두지 않음
    
    오늘 회의에서 프론트랑 어떻게 연계하고 검증하는지 말해줄것임

---
+++ back/src/user/users.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './entities/user.entity';
+import * as bcrypt from 'bcrypt';
+
+@Injectable()
+export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private usersRepository: Repository<User>,
+  ) {}
+
+  async create(user: User): Promise<User> {
+    user.password = await bcrypt.hash(user.password, 10);
+    return this.usersRepository.save(user);
+  }
+  
+  async findOne(id: string): Promise<User> {
+    return this.usersRepository.findOne({ 
+      where: { id: id },
+     });
+  }
+
+  // TODO: more methods
+}
