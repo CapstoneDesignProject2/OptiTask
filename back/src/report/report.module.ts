f0481068a72d9fc44b24e054342f0357dbd974fd
us4c0d3 <jwseok312@gmail.com>
2023-11-27 10:33:25 +0900

    Feat: add project and report entity to module

--- back/src/report/report.module.ts
+++ back/src/report/report.module.ts
@@ -1,4 +1,12 @@
 import { Module } from '@nestjs/common';
+// import { TodoController } from './todo.controller';
+// import { TodoService } from './todo.service';
+import { Report } from './report.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
-@Module({})
+@Module({
+    imports: [TypeOrmModule.forFeature([Report])],
+    // controllers: [TodoController],
+    // providers: [TodoService],
+})
 export class ReportModule {}
