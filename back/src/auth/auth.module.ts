6636817dcb1ad50cb9540962d84e16b26727f32e
us4c0d3 <jwseok312@gmail.com>
2023-11-27 15:42:49 +0900

    fix: UserModule, AuthModule, JwtModule 간 의존성 오류 해결

--- back/src/auth/auth.module.ts
+++ back/src/auth/auth.module.ts
@@ -1,11 +1,28 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
 import { LocalStrategy } from './local.strategy';
+import { JwtModule } from '@nestjs/jwt';
 import { UserModule } from '../user/users.module';
 import { PassportModule } from '@nestjs/passport';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [UserModule, PassportModule],
+  imports: [
+    UserModule,
+    PassportModule,
+    JwtModule.registerAsync({
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: {
+          expiresIn: configService.get<string>('JWT_EXPIRATION'),
+          algorithm: 'HS256',
+        },
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+  controllers: [AuthController],
   providers: [AuthService, LocalStrategy],
 })
 export class AuthModule {}
