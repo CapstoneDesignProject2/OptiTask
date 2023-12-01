4f5350fee6ecd1cc70c60e787854b0b230adef1a
us4c0d3 <jwseok312@gmail.com>
2023-12-02 02:53:55 +0900

    refactor: actions 실행 조건 및 브랜치 변경 / main.ts port숨김

--- back/src/main.ts
+++ back/src/main.ts
@@ -3,11 +3,13 @@ import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config'; // .env
 
 async function bootstrap() {
-	const configService = new ConfigService();
-  	const app = await NestFactory.create(AppModule);
-  	const port = configService.get<number>('PORT');
-	app.enableCors();
-  	await app.listen(3000);
- 	console.log(`Server is running on port ${port}`);
+  const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService); // .env
+  const port = configService.get<number>('PORT');
+
+  app.enableCors();
+  await app.listen(port);
+
+  console.log(`Server is running on port ${port}`);
 }
 bootstrap();
