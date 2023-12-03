b566e5d26536d19b3c19b0a688bd70b8006c8a9a
kalteon <tnghdjaak75@gmail.com>
2023-12-03 21:12:23 +0900

    feature report repository and service

--- back/src/report/report.controller.ts
+++ back/src/report/report.controller.ts
@@ -1,16 +1,20 @@
-// import { Controller, Get, Param } from '@nestjs/common';
-// import { ReportService } from './report.service';
+import { Controller, Get, Param } from '@nestjs/common';
+import { ReportService } from './report.service';
 
-// @Controller('report')
-// export class ReportController {
-//   constructor(private readonly reportService: ReportService) {}
+@Controller('report')
+export class ReportController {
+  constructor(private readonly reportService: ReportService) {}
 
-//   @Get(':pastWeeksAgo')
-//   findWeeklyReport(@Param('pastWeeksAgo') pastWeeksAgo: number) {
-//     return this.reportService.findWeeklyReport(pastWeeksAgo);
-//   }
-//   @Get()
-//   findReportTrend() {
-//     return this.reportService.findReportTrend();
-//   }
-// }
+  @Get('reportId')
+  findWeeklyReport(@Param('reportId') reportId: number) {
+    return this.reportService.findWeeklyReport(reportId);
+  }
+  @Get('projectId')
+  findWeeklyReportsByProjectId(@Param('projectId') projectId: number) {
+    return this.reportService.findWeeklyReportsByProjectId(projectId)
+  }
+  @Get('/trend/:projectId')
+  findReportTrend(@Param('projectId') projectId: number) {
+    return this.reportService.findReportTrend(projectId);
+  }
+}
