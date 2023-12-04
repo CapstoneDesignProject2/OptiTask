e15df756b4e014ad1a660a02e5bcd3400829d3f8
hojun <ghwnsqkrkwl@naver.com>
2023-12-04 19:46:21 +0900

    no message

--- front/src/Pages/Login.js
+++ front/src/Pages/Login.js
@@ -1,10 +1,10 @@
-import React, { useEffect, useState} from "react";
 import axios from "axios";
-import {useNavigate} from 'react-router-dom';
+import React, { useState } from "react";
+import { useNavigate } from 'react-router-dom';
 import Button from '../Components/Button';
 import Title from '../Components/Title';
 
-function Login({}) {
+function Login({ }) {
     const navigate = useNavigate();
     const [userId, setUserId] = useState(null);
     const [id, setID] = useState("");
@@ -18,32 +18,32 @@ function Login({}) {
     }
 
     const handleSignIn = async (id, pw) => {
-        try{
-            const response = await axios.post("/auth/login", {id, pw});
+        try {
+            const response = await axios.post("/auth/login", { id, pw });
             const data = response.data;
 
             if (data.access_token) {
-                document.cookie = `session_id=${result.uid}`;
+                document.cookie = `session_id=${data.access_token}`;
                 goToHome(); // 성공 시 홈 페이지로 이동
             } else {
                 // 로그인 실패 처리
                 alert("Login failed");
             }
-        } catch(error) {
+        } catch (error) {
             console.error("Login error", error);
             alert("Login Failed");
         }
     };
 
-    return(
+    return (
         <div>
             <Title></Title>
             <input type="text" value={id} onChange={(e) => setID(e.target.value)} placeholder="ID" style={{ width: '400px', height: '50px', fontSize: '20px', marginBottom: '15px' }} />
             <br />
-            <input type="password" value={pw} onChange={(e) => setPW(e.target.value)}placeholder="PW" style={{ width: '400px', height: '50px', fontSize: '20px', marginBottom: '15px' }} />
+            <input type="password" value={pw} onChange={(e) => setPW(e.target.value)} placeholder="PW" style={{ width: '400px', height: '50px', fontSize: '20px', marginBottom: '15px' }} />
             <br />
-            <Button type="submit" text = "Sign In" style={{ width: '150px', height: '50px', fontSize: '20px', marginBottom: '15px' }}></Button>
-            <Button type="submit" text = "Create Account" onClick={goToJoin} style={{ width: '150px', height: '50px', fontSize: '20px', marginBottom: '15px' }}></Button>
+            <Button type="submit" text="Sign In" style={{ width: '150px', height: '50px', fontSize: '20px', marginBottom: '15px' }}></Button>
+            <Button type="submit" text="Create Account" onClick={goToJoin} style={{ width: '150px', height: '50px', fontSize: '20px', marginBottom: '15px' }}></Button>
         </div>
 
     );
