b8140ef5cc5e7501ae3b7de9573aadaaf2af83dc
BusanGukbap <dong2857210@gmail.com>
2023-12-17 19:56:01 +0900

    fix: localhost -> backend

--- front/src/Pages/Join.js
+++ front/src/Pages/Join.js
@@ -8,8 +8,8 @@ function Join() {
     const navigate = useNavigate();
     const [id, setID] = useState("");
     const [pw, setPW] = useState("");
+    const [pwcheck, setPWCheck] = useState("");
     
-
     const goToLogin = () => {
         navigate("/login");
     }
@@ -53,15 +53,22 @@ function Join() {
 
     const handleSignUp = async () => {
         try{
+            if (pw != pwcheck){
+                alert("비밀번호가 다릅니다");
+                return;
+            }
+
             const reqdata = {
                 id: id,
                 password: pw,
             };
             console.log(reqdata);
-            const response = await axios.post("http://localhost:3000/users/signup", JSON.stringify(reqdata), {
+            const response = await axios.post("http://backend:3000/users/signup", JSON.stringify(reqdata), {
                 headers: { "Content-Type": "application/json" },
             });
             console.log("response: ", response);
+
+            // 회원가입이 성공되었는지 확인
             if (response.status === 201){
                 goToLogin(); // 회원가입했으니 로그인하러 가기
             }
@@ -82,6 +89,8 @@ function Join() {
             <input type="text" value={id} onChange={(e) => setID(e.target.value)} placeholder="ID" style={styles.input} />
             <br />
             <input type="password" value={pw} onChange={(e) => setPW(e.target.value)} placeholder="PW" style={styles.input} />
+            <br />
+            <input type="password" value={pwcheck} onChange={(e) => setPWCheck(e.target.value)} placeholder="PW Check" style={styles.input} />
             <br />
             <button type="submit" text = "Join" onClick={handleSignUp} style={styles.button}>Sign Up</button>
         </div>
