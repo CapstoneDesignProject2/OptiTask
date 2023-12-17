546b2caa16e1c8a5e5844cef49f93e03657ae5f5
BusanGukbap <dong2857210@gmail.com>
2023-12-18 07:10:43 +0900

    feat: make logout and change password

--- front/src/Pages/Mypage.js
+++ front/src/Pages/Mypage.js
@@ -1,56 +1,116 @@
 import React, { useEffect, useState} from "react";
 import {useNavigate} from 'react-router-dom';
+import axios from 'axios';
 import Title from '../Components/Title';
 
 function Mypage({}) {
+    const navigate = useNavigate();
+    const [id, setID] = useState('');
+    const [showPasswordChange, setShowPasswordChange] = useState(false);
+    const [currentPassword, setCurrentPassword] = useState('');
+    const [newPassword, setNewPassword] = useState('');
+    const [confirmNewPassword, setConfirmNewPassword] = useState('');
 
+    const logout = () => {
+        localStorage.clear();
+        navigate('/login');
+    }
 
-    const [userInfo, setUserInfo] = useState(null);
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(null);
-    
-    // 사용자 정보를 가져오는 함수
-    const fetchUserInfo = async () => {
-        try {
-            const response = await fetch('/api/user-info'); // 임시로 넣어둠
-    
-            if (!response.ok) {
-                throw new Error('Network response was not ok ' + response.statusText);
-            }
-
-            const data = await response.json();
-            setUserInfo(data); // 사용자 정보를 상태에 저장합니다.
-        } catch (err) {
-            setError(err.message); // 오류 메시지를 상태에 저장합니다.
-        } finally {
-            setLoading(false); // 로딩 상태를 false로 설정합니다.
+    const handleChangePassword = async () => {
+        if (newPassword !== confirmNewPassword) {
+            alert('새 비밀번호가 일치하지 않습니다.');
+            return;
         }
+        if (newPassword === currentPassword) {
+            alert('현재 비밀번호와 동일합니다');
+            return;
+        }
+
+        try {
+            const reqdata = {
+                id: id,
+                newPassword: newPassword,
+            };
+            console.log(reqdata);
+
+            const response = await axios.post('https://optitask.site/api/users/modify', JSON.stringify(reqdata), {
+                headers: {"Content-Type": `application/json`},
+            });
+            console.log("response:", response);
+
+            if (response.status === 201) {
+                alert('비밀번호가 변경되었습니다.');
+                navigate('/');
+            } else {
+                alert('비밀번호 변경에 실패했습니다.');
+            }
+        } catch (error) {
+            console.error('비밀번호 변경 요청 실패', error);
+            alert("password change error");
+        }
+    }
+
+    const styles = {
+        container: {
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            justifyContent: 'space-around', // 요소들 사이의 공간을 균등하게 분배
+            height: '100vh',
+            color: 'black',
+            backgroundColor: 'white',
+            padding: '20px',
+            maxWidth: '1200px', // 컨테이너 최대 너비 설정
+            margin: '0 auto', // 가운데 정렬
+        },
+        projectContainer: {
+            display: 'flex',
+            justifyContent: 'space-around', // 프로젝트 사이의 공간을 균등하게 분배
+            width: '100%', // 전체 너비 사용
+            flexWrap: 'wrap', // 요소가 너무 많으면 다음 줄로 넘어감
+        },
     };
-    
-    useEffect(() => {
-        fetchUserInfo();
-    }, []);
-    
-    // 로딩 상태, 에러 상태, 또는 사용자 정보에 따라 적절한 UI를 렌더링합니다.
-    if (loading) {
-        return <div>Loading...</div>;
-    }
-    if (error) {
-        return <div>Error: {error}</div>;
-    }
-     
+
+
+
     return (
-        <div>
+        <div style={styles.container}>
             <Title></Title>
-        {userInfo && (
-            <div>
-            <p>Name: {userInfo.name}</p>
-            <p>Email: {userInfo.email}</p>
-            {/* 추가적으로 표시하고 싶은 사용자 정보를 여기에 추가합니다. */}
-            </div>
-        )}
+            <button onClick={logout}>logout</button>
+            <button onClick={() => setShowPasswordChange(!showPasswordChange)}>
+                비밀번호 변경
+            </button>
+            {showPasswordChange && (
+                <div>
+                    <input
+                        type="text"
+                        placeholder="ID"
+                        value={id}
+                        onChange={(e) => setID(e.target.value)}
+                    />
+                    <input
+                        type="password"
+                        placeholder="Current Password"
+                        value={currentPassword}
+                        onChange={(e) => setCurrentPassword(e.target.value)}
+                    />
+                    <input
+                        type="password"
+                        placeholder="New Password"
+                        value={newPassword}
+                        onChange={(e) => setNewPassword(e.target.value)}
+                    />
+                    <input
+                        type="password"
+                        placeholder="Confirm New Password"
+                        value={confirmNewPassword}
+                        onChange={(e) => setConfirmNewPassword(e.target.value)}
+                    />
+                    <button onClick={handleChangePassword}>Change Password</button>
+                </div>
+            )}
         </div>
     );
-};
+}
     
 export default Mypage;
