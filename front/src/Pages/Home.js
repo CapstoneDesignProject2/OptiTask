0c5704a8bbc9abfb509cd187806014e0a519d55f
hojun <ghwnsqkrkwl@naver.com>
2023-12-17 17:13:52 +0900

    no message

--- front/src/Pages/Home.js
+++ front/src/Pages/Home.js
@@ -1,6 +1,7 @@
+import axios from "axios";
+import { jwtDecode } from "jwt-decode";
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import Title from '../Components/Title';
 
 
 function Home() {
@@ -8,6 +9,8 @@ function Home() {
     const [projects, setProjects] = useState([]);
     const [currentIndex, setCurrentIndex] = useState(0);
     const projectsPerPage = 3;
+    const [userId, setUserId] = useState(null);
+    const [isLoading, setIsLoading] = useState(true);
 
     const styles = {
         container: {
@@ -61,29 +64,57 @@ function Home() {
             borderRadius: '5px', // 버튼의 모서리 둥글게
             marginTop: '-180px'
         },
+
+        header: {
+            color: '#333', // 글자색 설정
+            fontSize: '70px', // 글자 크기 설정
+            textAlign: 'center', // 글자를 가운데 정렬
+            margin: '0', // 위아래 여백 제거
+            padding: '20px 0', // 위아래 패딩 추가
+            marginTop: '-80px'
+        },
+
+
+
+
+
+
         // ...나머지 스타일 유지
     };
 
     const [isLoggedIn, setIsLoggedIn] = useState(false);
-    
+
     const goToLogin = () => {
         navigate("/login");
     }
 
     useEffect(() => {
-        // 사용자의 로그인 상태를 확인합니다.
+        // 사용자의 로그인 상태를 확인
         const token = localStorage.getItem('access_token');
-        if (!token) {
-            // 토큰이 없으면 로그인 페이지로 리디렉션합니다.
+        if (token) {
+            const decoded = jwtDecode(token);
+            setUserId(decoded.userId);
+            // 서버로부터 프로젝트 데이터 가져오기
+            axios.get(`http://localhost:3000/project/${decoded.userId}`)
+                .then(response => {
+                    // 성공적으로 데이터를 받아오면 state 업데이트
+                    setProjects(response.data.AllProjects); // 응답 구조에 따라 변경될 수 있습니다.
+                })
+                .catch(error => {
+                    console.error('Error fetching projects', error);
+                });
+
+            setIsLoading(false);
+        } else {
+            // 토큰이 없으면 로그인 페이지로 리디렉션
             navigate('/login');
         }
-
-        // 서버로부터 프로젝트 데이터를 가져오는 로직
-        // 데이터를 최신순으로 정렬
     }, [navigate]);
 
+
     const handleProjectClick = (projectId) => {
-        navigate(`/project/${projectId}`);
+        navigate(`/project/${userId}/${projectId}`);
+
     };
 
     const nextProjects = () => {
@@ -103,13 +134,13 @@ function Home() {
 
     return (
         <div style={styles.container}>
-            <Title></Title>
-            <button style={styles.create_button} onClick={() => navigate('/Projectcreate')}>Create New Project</button>
+            <h1 style={styles.header}>OptiTask</h1>
+            <button style={styles.create_button} onClick={() => navigate(`/ProjectCreate/${userId}`)}>Create New Project</button>
             <div style={styles.projectContainer}>
                 {currentProjects.map(project => (
-                    <div key={project.id} onClick={() => handleProjectClick(project.id)} style={styles.projectCard}>
-                        <h3>{project.name}</h3>
-                        <p>Created on: {project.creationDate}</p>
+                    <div key={project.projectId} onClick={() => handleProjectClick(project.projectId)} style={styles.projectCard}>
+                        <h3>{project.projectName}</h3>
+                        <p>Created on: {project.deadline}</p>
                     </div>
                 ))}
             </div>
