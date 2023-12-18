7c3a469130cb19653f652a67c68d0c2e9836d2b1
BusanGukbap <dong2857210@gmail.com>
2023-12-19 08:12:27 +0900

    fix: add useNavigate

--- front/src/Pages/WeeklyReport.js
+++ front/src/Pages/WeeklyReport.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import Title from '../Components/Title';
 
 const WeeklyReport = () => {
