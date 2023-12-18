3a1625598a1d43bf36b71496cd22aa469f2bb2f3
BusanGukbap <dong2857210@gmail.com>
2023-12-19 08:53:18 +0900

    test: gpt 생성 초기버전

--- front/src/Pages/WeeklyReport.js
+++ front/src/Pages/WeeklyReport.js
@@ -4,34 +4,57 @@ import { useNavigate, useParams } from 'react-router-dom';
 import Title from '../Components/Title';
 
 const WeeklyReport = () => {
-    const navigate = useNavigate();
     const { projectId } = useParams();
-    
+    const [reports, setReports] = useState([]);
+    const [reportTrend, setReportTrend] = useState({});
+    const [advice, setAdvice] = useState('');
+    const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
-        axios.post('http://localhost:3000/report');
-        //  weakly reports by projectid
-        axios.get(`http://localhost:3000/report/${projectId}`)
-            .then(response => {
-                console.log(response.data);
+        // 주간 보고서 생성
+        axios.post('/report')
+            .then(() => {
+                // 특정 프로젝트의 모든 주간 보고서 조회
+                return axios.get(`/report/${projectId}`);
             })
+            .then(response => {
+                setReports(response.data);
+                // 프로젝트의 추세 데이터 조회
+                return axios.get(`/report/trend/${projectId}`);
+            })
+            .then(response => {
+                setReportTrend(response.data);
+                // 추세에 대한 조언 조회
+                return axios.get(`/report/trend/${projectId}/advice`);
+            })
+            .then(response => {
+                setAdvice(response.data);
+                setIsLoading(false);
+            })
+            .catch(error => {
+                console.error('Error fetching report data:', error);
+                setIsLoading(false);
+            });
+    }, [projectId]);
 
-        // find report trend
-        axios.get(`http://localhost:3000/report/trend/${projectId}`)
-            .then(response=> {
-                console.log(response.data);
-            })
-        //  find adivce for report trend
-        axios.get(`http://localhost:3000/report/trend/${projectId}/advice`)
-            .then(response => {
-                console.log(response.data);
-            })
-    });
-    
+    if (isLoading) {
+        return <div>Loading...</div>;
+    }
 
     return (
         <div>
             <Title></Title>
+            {reports.map((report, index) => (
+                <div key={index}>
+                    <h2>{`Week ${report.reportWeek}`}</h2>
+                    <p>{`Total Time: ${report.weeklyTotalTime}`}</p>
+                    <p>{`Success Todos: ${report.successTodo}`}</p>
+                </div>
+            ))}
+            <h2>프로젝트 추세</h2>
+            {/* reportTrend 데이터를 사용하여 추세 정보 표시 */}
+            <h2>추세에 대한 조언</h2>
+            <p>{advice}</p>
         </div>
     );
 };
