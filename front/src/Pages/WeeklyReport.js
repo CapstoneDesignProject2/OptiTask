d034b3222f1aa32bbaa8fba75ba8c91425386cec
BusanGukbap <dong2857210@gmail.com>
2023-12-19 08:04:26 +0900

    Revert "test: weakly report"
    This reverts commit c202621f069ce9a59cf4d144e65d18154b99b590.

--- front/src/Pages/WeeklyReport.js
+++ front/src/Pages/WeeklyReport.js
@@ -1,38 +1,44 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { useNavigate, useParams } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import Title from '../Components/Title';
 
 const WeeklyReport = () => {
-    const navigate = useNavigate();
-    const { projectId } = useParams();
-    
+    const { userId, projectId } = useParams();
+    const [reports, setReports] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
     useEffect(() => {
-        axios.post('http://localhost:3000/report');
+        const fetchReports = async () => {
+            try {
+                const response = await axios.get(`https://optitask.site/api/report/${userId}/${projectId}`);
+                setReports(response.data);
+                setLoading(false);
+            } catch (err) {
+                setError(err);
+                setLoading(false);
+            }
+        };
 
-        //  weakly reports by projectid
-        axios.get(`http://localhost:3000/report/${projectId}`)
-            .then(response => {
-                console.log(response.data);
-            })
+        fetchReports();
+    }, [userId, projectId]);
 
-        // find report trend
-        axios.get(`http://localhost:3000/report/trend/${projectId}`)
-            .then(response=> {
-                console.log(response.data);
-            })
-        //  find adivce for report trend
-        axios.get(`http://localhost:3000/report/trend/${projectId}/advice`)
-            .then(response => {
-                console.log(response.data);
-            })
-    });
-
+    if (loading) return <div>Loading...</div>;
+    if (error) return <div>Error: {error.message}</div>;
 
     return (
         <div>
             <Title></Title>
-            
+            <ul>
+                {reports.map(report => (
+                    <li key={report.reportId}>
+                        <div>주차: {report.reportWeek}</div>
+                        <div>총 작업 시간: {report.weeklyTotalTime}시간</div>
+                        <div>성공한 할 일 수: {report.successTodo}</div>
+                    </li>
+                ))}
+            </ul>
         </div>
     );
 };
